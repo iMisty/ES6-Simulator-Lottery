6392ba9e3d31a063a3171aec8549470da507245e
Jingyi21 <qianwumeng_2cyndi@qq.com>
2018-10-05 19:11:46 +0800

    Remove

--- app/js/class/index.js
+++
@@ -1,9 +0,0 @@
-class Test{
-    constructor(){
-        this.a = 'hello world'
-    }
-}
-
-let test = new Test();
-
-document.body.innerHTML = test.a;
--- tasks/css.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp';
-import gulpIf from 'gulp-if';
-import liveReload from 'gulp-livereload';
-import args from './util/args';
-
-gulp.task('css',()=>{
-    return gulp.src('app/**/*.css')
-        .pipe(gulp.dest('server/public'))
-});
